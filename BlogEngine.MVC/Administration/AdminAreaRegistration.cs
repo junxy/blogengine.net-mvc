f5a89f76b8efcd242caa8a911c6355a347ac8bf9
junxy <v214@vip.qq.com>
2011-10-24 13:00:58 +0800

    add admin

---
+++ BlogEngine.MVC/Administration/AdminAreaRegistration.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Mvc;
+
+namespace BlogEngine.Admin
+{
+    public class AdminAreaRegistration : AreaRegistration
+    {
+        public override string AreaName
+        {
+            get
+            {
+                return "Admin";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapRoute(
+                "Admin_default",
+                "Admin/{controller}/{action}/{id}",
+                new { controller = "Home", action = "Index", area = "Admin", id = "" },
+                new[] { "BlogEngine.Admin.Controllers" }
+            );
+        }
+    }
+}
